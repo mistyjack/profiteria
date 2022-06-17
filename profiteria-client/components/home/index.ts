22882dea5d66b31d5a165616ea72d6c9eed57b7b
mistyjack <oladepomustafa@gmail.com>
2022-06-17 10:58:49 +0100

    Fix how it works

--- profiteria-client/components/home/index.ts
+++ profiteria-client/components/home/index.ts
@@ -2,5 +2,5 @@ export { default as Hero } from "./Hero";
 export { default as WhatYouGet } from "./WhatYouGet";
 export { default as LeadingBit } from "./LeadingBit";
 export { default as HireTrader } from "./HireTrader";
-export { default as HowItWorks } from "./HowItWorks";
+export { default as HowItWorks } from "./howItWorks";
 export { default as GetStartedInput } from "./GetUpdateInput";
